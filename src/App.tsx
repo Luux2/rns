c0b4d044e69a9bb63453e244bda07833a4b0b943
Olsen7351 <olsen7351@gmail.com>
2024-12-10 18:53:07 +0100

    Needs to be there for it to deploy correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import './App.css'
-import {HelmetProvider} from "react-helmet-async";
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import IndexScreen from './pages/IndexScreen.tsx'
+import "./App.css";
+import { HelmetProvider } from "react-helmet-async";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import IndexScreen from "./pages/IndexScreen.tsx";
 import TournamentScreen from "./pages/TournamentScreen.tsx";
 
 function App() {
-
   return (
-      <HelmetProvider>
-        <BrowserRouter>
-            <Routes>
-                <Route index path="/" element={<IndexScreen/>}/>
-                <Route path="/turnering" element={<TournamentScreen/>}/>
-            </Routes>
-        </BrowserRouter>
-      </HelmetProvider>
-  )
+    <HelmetProvider>
+      <BrowserRouter basename="/rns/">
+        <Routes>
+          <Route index path="/" element={<IndexScreen />} />
+          <Route path="/turnering" element={<TournamentScreen />} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
+  );
 }
 
-export default App
+export default App;
