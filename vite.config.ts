aa3ab46493585344b5463e8d01c62340232654bd
Luux2 <293428@viauc.dk>
2024-12-10 14:04:46 +0100

    PWA og deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/rns/',
   plugins: [
     react(),
     VitePWA({
@@ -31,5 +32,4 @@ export default defineConfig({
       },
     }),
   ],
-  base: '/rns/',
 });
