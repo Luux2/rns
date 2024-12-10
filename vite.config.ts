630e2773a74ce3ce3e62eda7c99f17e49cf35dfa
Luux2 <293428@viauc.dk>
2024-12-10 13:04:57 +0100

    PWA og deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: "/rns/",
+  base: '/rns/', // Dit GitHub repository navn
   plugins: [
     react(),
     VitePWA({
@@ -12,20 +12,14 @@ export default defineConfig({
       manifest: {
         name: "Rise 'n Shine",
         short_name: 'RNS',
+        start_url: '/rns/', // Matcher base
+        scope: '/rns/',     // Matcher base
         display: 'standalone',
         background_color: '#ffffff',
         theme_color: '#000000',
         icons: [
-          {
-            src: 'rns192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'rns512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
+          { src: 'rns192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'rns512.png', sizes: '512x512', type: 'image/png' },
         ],
       },
     }),
