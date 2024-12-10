36493f30aa421c5c75f2c2bf8ee8c0b0866a8d3a
Luux2 <293428@viauc.dk>
2024-12-10 12:15:13 +0100

    PWA og deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,35 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
-import {VitePWA} from "vite-plugin-pwa";
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    base: '/rns/',
-    registerType: 'autoUpdate',
-    injectRegister: 'auto',
-    manifest: {
-      name: "Rise 'n Shine",
-      short_name: 'RNS',
-      start_url: '/rns/',
-      display: 'standalone',
-      background_color: '#ffffff',
-      theme_color: '#000000',
-      scope: '/rns/',
-      icons: [
-        {
-          src: 'rns192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: 'rns512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
-    },
-  })],
-})
+  base: '/rns/',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      manifest: {
+        name: "Rise 'n Shine",
+        short_name: 'RNS',
+        start_url: '/rns/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        scope: '/rns/',
+        icons: [
+          {
+            src: 'rns192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'rns512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
